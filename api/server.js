565cc4bd2502b95861a4855d60c6f5b8d9178c21
Roman <Roman.Vengl1@gmail.com>
2020-05-02 17:14:42 +0300

    added morgan and handleErrors

--- api/server.js
+++ api/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const morgan = require("morgan");
 require("dotenv").config();
 const port = process.env.PORT;
 
@@ -14,6 +15,7 @@ module.exports = class ContactsServer {
     this.initServer();
     this.initMiddlewares();
     this.initRoutes();
+    this.handleErrors();
     this.startListening();
   }
 
@@ -24,10 +26,18 @@ module.exports = class ContactsServer {
   initMiddlewares() {
     this.server.use(express.json());
     this.server.use(cors({ origin: "http://localhost:3000" }));
+    this.server.use(morgan("tiny"));
   }
 
   initRoutes() {
     this.server.use("/api/contacts", contactsRouter);
+  }
+
+  handleErrors() {
+    this.server.use((err, req, res, next) => {
+      delete err.stack;
+      return res.status(err.status).send(`${err.name}: ${err.message}`);
+    });
   }
 
   startListening() {
